afef266a41d2b53c9c74a7fd2432e48af8ecabb9
Mohamed Amged <mohamedamged55555@gmail.com>
2024-10-08 08:37:38 +0300

    try fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './App.css'
+import MyFooter from './components/myFooterSec/MyFooter'
 import MyLayout from './components/myLayoutSecc/MyLayout'
+import MyNavBar from './components/myNavBarSecc/MyNavbar'
 function App() {
 
+  const [scrollToggle, setScrollToggle] = useState(false);
 
+  window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+          setScrollToggle(true);
+      } else {
+          setScrollToggle(false);
+      }
+  });
   return (
     <>
+      <MyNavBar scrollToggle={scrollToggle}/>
       <MyLayout />
+      <MyFooter />
+
     </>
   )
 }
--- src/components/myLayoutSecc/MyLayout.jsx
+++ src/components/myLayoutSecc/MyLayout.jsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import MyHome from "../../pages/myHomePage/MyHome";
-import MyFooter from "../myFooterSec/MyFooter";
+// import MyFooter from "../myFooterSec/MyFooter";
 import MyShop from "../../pages/myShopPage/MyShop";
-import MyNavBar from "../myNavBarSecc/MyNavbar";
+// import MyNavBar from "../myNavBarSecc/MyNavbar";
 
 export default function MyLayout() {
-    const [scrollToggle, setScrollToggle] = useState(false);
 
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 200) {
-            setScrollToggle(true);
-        } else {
-            setScrollToggle(false);
-        }
-    });
     return (
         <>
-            <MyNavBar scrollToggle={scrollToggle}/>
+            
             <Routes>
                 <Route path='/' element={<MyHome />} />
                 <Route path='/shop' element={<MyShop />} />
             </Routes>
-            <MyFooter />
         </>
     )
 }
